d08fcf074807f59682f073d4eb6b584852acb454
codelife-us <106282560+codelife-us@users.noreply.github.com>
2025-10-02 12:49:08 -0500

    Update OriginalAmountFromDiscountedAmount.py
    changed warning line for perecentage input and added additional comment at top

--- OriginalAmountFromDiscountedAmount.py
+++ OriginalAmountFromDiscountedAmount.py
@@ -1,4 +1,5 @@
 # What is the original amount given a discounted percentage and known amount charged
+# Show amount saved
 import sys
 try:
     discounted_amount = float(input("What is the discounted amount? "))
@@ -14,10 +15,11 @@ except ValueError:
     print("Invalid input. Enter a numerical amount.")
     sys.exit(1)
 if percentage >= 100 or percentage < 0:
-    print("Percentage must be greater than zero and less than one hundred.")
+    print("Percentage must be zero or greater and less than one hundred.")
     sys.exit(1)
 else:    
     original_amount = discounted_amount * (1 / (1 - percentage / 100))
     print(f"The original amount would have been ${original_amount:,.2f}.")
     savings_amount = original_amount - discounted_amount
+
     print(f"The savings amount is ${savings_amount:,.2f}.")
